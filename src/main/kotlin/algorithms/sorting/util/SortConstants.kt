4715961dcc71e9690ab4e70ab02ede285d8cd1bb
earl <esinilyadev@gmail.com>
2023-06-20 08:40:32 +0500

    Quick sort algo

---
+++ src/main/kotlin/algorithms/sorting/util/SortConstants.kt
@@ -0,0 +1,8 @@
+package algorithms.sorting.util
+
+object SortConstants {
+
+    val mutableListToSort = mutableListOf(3, 6, 1, 4, 89, 23, 56, 90, 132, 5, 8, 45, 67)
+    val arrayToSort = arrayOf(3, 6, 1, 4, 89, 23, 56, 90, 132, 5, 8, 45, 67)
+
+}
